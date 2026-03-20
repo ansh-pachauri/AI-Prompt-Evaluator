8a2517b4eb5d34ff98481aff86544f13a1d27ba1
Answerwriting <integrations.answerwriting@gmail.com>
2026-03-20 22:59:30 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "@/src/utils/Provider";
-
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +31,7 @@ export default function RootLayout({
       >
         <ReactQueryProvider>
           {children}
+          <Analytics/>
         </ReactQueryProvider>
     
       </body>
